ebaf9307294b03008ba44c91aacca0cda9fc0db6
rohit-r7 <reghunathrohit@gmail.com>
2023-01-03 11:26:09 +0530

    cart and order updates

--- customer/urls.py
+++ customer/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
    path('customer_home',views.index, name = 'customer_home'),
    path('product_details/',views.product_details, name = 'product_details'),
    path('change_password/',views.change_password, name = 'change_password'),
+   path('my_cart/',views.cart, name = 'cart'),
+   path('my_orders/',views.order, name = 'orders'),
    
 ]
--- customer/views.py
+++ customer/views.py
@@ -11,6 +11,12 @@ def product_details(request) :
 def change_password(request) :
     return render (request,'customer/change_password.html')
 
+def cart(request) :
+    return render (request,'customer/cart.html')
+
+def order(request) :
+    return render (request,'customer/order.html')
+
 
 
 
